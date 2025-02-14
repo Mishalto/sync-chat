3bef69bd29879806dfa1b145c3b7337cf3fef1bc
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-02-15 00:56:52 +0300

    Error handling added

--- Client/main.cpp
+++ Client/main.cpp
@@ -7,8 +7,9 @@ using boost::asio::ip::tcp;
 // Server port
 const int port = 12345;
 
-// Creates an io_context to manage
+// Creates an io_context to manage and boost::system::error_code for error handling
 boost::asio::io_context io_context;
+boost::system::error_code ec;
 
 // Server addr
 tcp::endpoint ep(boost::asio::ip::make_address_v4("127.0.0.1"), port);
@@ -17,7 +18,10 @@ int main()
 {
     // Trying to connect to the server
     auto socket_ptr = std::make_shared<tcp::socket>(io_context);
-    socket_ptr->connect(ep);
+    socket_ptr->connect(ep, ec);
+    if(ec){
+        std::cerr << "Connection error: " << ec.what() << '\n';
+    }
 
     // Confirm connect
     std::cout << "Connected\n";
--- Server/main.cpp
+++ Server/main.cpp
@@ -7,8 +7,9 @@ using boost::asio::ip::tcp;
 // Listening this port
 const int port = 12345;
 
-// Creates an io_context to manage
+// Creates an io_context to manage and boost::system::error_code for error handling
 boost::asio::io_context io_context;
+boost::system::error_code ec;
 
 // Acceptor settings
 tcp::endpoint ep(tcp::v4(), port);
@@ -19,7 +20,10 @@ int main()
     // Start accepting connections
     auto socket_ptr = std::make_shared<tcp::socket>(io_context);
     std::cout << "Waiting client...\n";
-    acceptor.accept(*socket_ptr);
+    acceptor.accept(*socket_ptr, ec);
+    if(ec){
+        std::cerr << "Accept failed: " << ec.what() << '\n';
+    }
 
     // Confirm connection
     std::cout << "Client connected\n";
